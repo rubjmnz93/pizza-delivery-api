acdda49765201222ce050b4b998d1927a516635e
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2024-08-28 17:51:36 +0200

    Added create user POST method.

--- pizza/authentication/views.py
+++ pizza/authentication/views.py
@@ -1,7 +1,22 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from . import serializers
 
 
 class HelloAuthView(generics.GenericAPIView):
     def get(self, request):
         return Response(data={"message": "Hello Auth"}, status=status.HTTP_200_OK)
+
+
+class UserCreateView(generics.GenericAPIView):
+    serializer_class = serializers.UserCreationSerializer
+
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
