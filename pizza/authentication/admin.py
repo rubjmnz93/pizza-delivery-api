d8555c381b27c5ff4e42789233b353f60270cb82
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2024-08-28 17:27:01 +0200

    Upgraded User admin view.

--- pizza/authentication/admin.py
+++ pizza/authentication/admin.py
@@ -1,4 +1,80 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "username"]
+    fieldsets = (
+        (
+            None,
+            {"fields": ("email", "username", "phone_number")},
+        ),
+        (
+            "Personal information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "date_joined",
+                    "last_login",
+                )
+            },
+        ),
+    )
+    readonly_fields = ["last_login", "date_joined"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "phone_number",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+        (
+            "Personal information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
--- pizza/authentication/models.py
+++ pizza/authentication/models.py
@@ -40,5 +40,7 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "phone_number"]
 
+    objects = CustomUserManager()
+
     def __str__(self):
-        return f"<User {self.email}"
+        return f"<User {self.email} - {self.username}>"
